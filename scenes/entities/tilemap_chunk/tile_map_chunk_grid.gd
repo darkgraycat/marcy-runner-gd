cfe2c42eb80ea323904d342d65c6fee409ea0d45
darkgraycat <jburyanov@gmail.com>
2025-06-09 19:50:31 +0300

    : add customization options of tilemap chunk grid

--- scenes/entities/tilemap_chunk/tile_map_chunk_grid.gd
+++ scenes/entities/tilemap_chunk/tile_map_chunk_grid.gd
@@ -1,40 +1,45 @@
 @tool
 class_name TileMapChunkGrid extends Node2D
 
+@export_group("Dimensions")
 ## Size of each tile in pixels (width x height)
-@export var pixels_per_tile: Vector2i = Vector2i(16, 16):
-	set(v): pixels_per_tile = v; _sync()
-
+@export var tile_size: Vector2i = Vector2i(16, 16):
+	set(v): tile_size = v; _sync_dimensions()
 ## Number of tiles in each chunk (width x height)
-@export var tiles_per_chunk: Vector2i = Vector2i(8, 8):
-	set(v): tiles_per_chunk = v; _sync()
-
+@export var chunk_size: Vector2i = Vector2i(8, 8):
+	set(v): chunk_size = v; _sync_dimensions()
 ## Number of chunks in the grid (horizontal x vertical)
-@export var chunks_per_grid: Vector2i = Vector2i(4, 4):
-	set(v): chunks_per_grid = v; _sync()
+@export var grid_size: Vector2i = Vector2i(4, 4):
+	set(v): grid_size = v; _sync_dimensions()
+
+@export_group("Visuals")
+## Color for odd chunks
+@export var grid_color_odd: Color = Color(0.5, 0.5, 0.2, 0.3):
+	set(v): grid_color_odd = v; _sync_visuals()
+## Color for even chunks
+@export var grid_color_even: Color = Color(0.2, 0.5, 0.5, 0.3):
+	set(v): grid_color_even = v; _sync_visuals()
+## Color for line separators
+@export var grid_line_color: Color = Color(0.2, 0.2, 0.2, 0.5):
+	set(v): grid_line_color = v; _sync_visuals()
+## Width of line separators
+@export var grid_line_width: float = 1.0:
+	set(v): grid_line_width = v; _sync_visuals()
 
 @onready var color_rect: ColorRect = $ColorRect
 @onready var shader: ShaderMaterial = color_rect.material
 
 func _ready() -> void:
 	if not Engine.is_editor_hint(): hide()
-	_sync()
-
-
-func _sync() -> void:
-	if not is_node_ready(): await ready
-	var rect_size := chunks_per_grid * tiles_per_chunk * pixels_per_tile
-	color_rect.size = rect_size
-	shader.set_shader_parameter("pixels_per_tile", pixels_per_tile)
-	shader.set_shader_parameter("tiles_per_chunk", tiles_per_chunk)
-	shader.set_shader_parameter("rect_size", rect_size)
+	_sync_dimensions()
+	_sync_visuals()
 
 
 func get_tilemap_layer_patterns(layer: TileMapLayer) -> Array[TileMapPattern]:
 	var result: Array[TileMapPattern] = []
 	var chunk_size := get_tilemap_layer_chunk_size(layer)
-	for y: int in chunks_per_grid.y:
-		for x: int in chunks_per_grid.x:
+	for y: int in grid_size.y:
+		for x: int in grid_size.x:
 			var pos := Vector2i(x, y) * chunk_size
 			var coords := recti_to_points(Rect2i(pos, chunk_size))
 			result.append(layer.get_pattern(coords))
@@ -42,7 +47,7 @@ func get_tilemap_layer_patterns(layer: TileMapLayer) -> Array[TileMapPattern]:
 
 
 func get_tilemap_layer_chunk_size(layer: TileMapLayer) -> Vector2i:
-	return (pixels_per_tile / layer.tile_set.tile_size) * tiles_per_chunk
+	return (tile_size / layer.tile_set.tile_size) * chunk_size
 
 
 func recti_to_points(rect: Rect2i) -> Array[Vector2i]:
@@ -51,3 +56,18 @@ func recti_to_points(rect: Rect2i) -> Array[Vector2i]:
 		for y in rect.size.y:
 			coords.append(Vector2i(rect.position.x + x, rect.position.y + y))
 	return coords
+
+
+func _sync_dimensions() -> void:
+	if not is_node_ready(): await ready
+	var rect_size := grid_size * chunk_size * tile_size
+	color_rect.size = rect_size
+	shader.set_shader_parameter("tile_size", tile_size)
+	shader.set_shader_parameter("chunk_size", chunk_size)
+	shader.set_shader_parameter("rect_size", rect_size)
+
+func _sync_visuals() -> void:
+	shader.set_shader_parameter("primary_color", grid_color_odd)
+	shader.set_shader_parameter("secondary_color", grid_color_even)
+	shader.set_shader_parameter("line_color", grid_line_color)
+	shader.set_shader_parameter("line_width", grid_line_width)
