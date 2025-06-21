fe48424135b8c837526f3e233654a2cff08bb320
darkgraycat <jburyanov@gmail.com>
2025-06-21 21:28:49 +0300

    : split player update cycle into separate private methods

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -7,29 +7,17 @@ class_name Player extends CharacterBody2D
 
 var input_move: float = 0.0
 var input_jump: bool = false
+
+var jump_in_progress: bool = false
 var current_state: String = "idle"
 
 func _physics_process(delta: float) -> void:
-	velocity.x = move_velocity * input_move
-
-	if input_move:
-		animated_sprite_2d.flip_h = input_move < 0
-
-	if input_jump:
-		if is_on_floor():
-			velocity.y = -jump_velocity
-	elif velocity.y < 0:
-		velocity.y /= 2
-
+	_handle_moving()
+	_handle_jumping()
+	_handle_animation()
+	move_and_slide()
 	if !is_on_floor():
 		velocity.y += Global.GRAVITY * delta
-
-	var new_state := get_current_state()
-	if current_state != new_state:
-		current_state = new_state
-		animated_sprite_2d.play(new_state)
-
-	move_and_slide()
 
 
 func get_current_state() -> StringName:
@@ -37,3 +25,29 @@ func get_current_state() -> StringName:
 		"jump" if not is_on_floor() else
 		"walk" if velocity.x else "idle"
 	)
+
+
+func _handle_moving() -> void:
+	velocity.x = move_velocity * input_move
+
+
+func _handle_jumping() -> void:
+	if input_jump and !jump_in_progress:
+		jump_in_progress = true
+		if is_on_floor():
+			velocity.y = -jump_velocity
+
+	if !input_jump:
+		jump_in_progress = false
+		if velocity.y < 0:
+			velocity.y /= 2
+
+
+func _handle_animation() -> void:
+	if input_move:
+		animated_sprite_2d.flip_h = input_move < 0
+
+	var next_state := get_current_state()
+	if current_state != next_state:
+		current_state = next_state
+		animated_sprite_2d.play(next_state)
