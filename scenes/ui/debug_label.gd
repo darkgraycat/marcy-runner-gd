e0a4ecea8ef886618fb97566f1516836bff7fcae
darkgraycat <jburyanov@gmail.com>
2025-08-03 09:17:44 +0300

    : add debug_label scene

---
+++ scenes/ui/debug_label.gd
@@ -0,0 +1,10 @@
+class_name DebugLabel extends Label
+
+@export var channel: int = 0
+
+func _ready() -> void:
+	Events.debug_message.connect(_on_debug_message)
+
+func _on_debug_message(message: String, ch: int) -> void:
+	if ch != 0 and ch != channel: return
+	text = message
--- scripts/events.gd
+++ scripts/events.gd
@@ -6,3 +6,6 @@ func emit_update_ui() -> void: update_ui.emit()
 
 signal effects_updated(effect_reciever: EffectReciever)
 func emit_effects_updated(effect_reciever: EffectReciever) -> void: effects_updated.emit(effect_reciever)
+
+signal debug_message(message: String, channel: int)
+func emit_debug_message(message: String, channel: int = 0) -> void: debug_message.emit(message, channel)
