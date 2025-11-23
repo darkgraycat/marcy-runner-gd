ac655264fd910bb9038f8e0097aff4f878ea82af
darkgraycat <jburyanov@gmail.com>
2025-11-23 09:06:26 +0200

    : removed old movement_component

--- components/movement/bonus_speed.component.gd
+++ components/movement/bonus_speed.component.gd
@@ -1,7 +1,7 @@
 class_name BonusSpeedComponent extends Component
 
 # variables #-------------------------------------------------------------------
-@export var movement_component: MovementComponent
+#@export var movement_component: MovementComponent
 @export var max_bonus_speed: float = 1000.0
 
 var bonus_speed: float = 0.0: set = set_bonus_speed
@@ -10,14 +10,14 @@ var parent_target_speed: float = 0.0
 # builtin #---------------------------------------------------------------------
 func _ready() -> void:
 	await parent.ready
-	parent_target_speed = movement_component.target_speed
+	#parent_target_speed = movement_component.target_speed
 	Utils.log("PARENT TS", parent_target_speed)
 
 # method #----------------------------------------------------------------------
 func set_bonus_speed(new_bonus_speed: float) -> void:
 	bonus_speed = min(new_bonus_speed, max_bonus_speed)
-	movement_component.target_speed = parent_target_speed + bonus_speed
-	Utils.log("SET BS %s, TS %s" % [bonus_speed, movement_component.target_speed])
+	#movement_component.target_speed = parent_target_speed + bonus_speed
+	#Utils.log("SET BS %s, TS %s" % [bonus_speed, movement_component.target_speed])
 
 # callback #--------------------------------------------------------------------
 
--- scenes/entities/enemy/enemy.gd
+++ scenes/entities/enemy/enemy.gd
@@ -1,20 +1,24 @@
 class_name Enemy extends CharacterBody2D
 
-# variables #-------------------------------------------------------------------
 @export var collision_shape_2d: CollisionShape2D
 @export var sprite_2d: Sprite2D
 @export var animation_player: AnimationPlayer
 @export var hurbox_area_2d: Area2D
 @export var status_effects: Array[StatusEffectResource] = []
+
+@onready var movement: Movement = $Movement
+
 var is_dying: bool = false
 
-# builtin #---------------------------------------------------------------------
 func _ready() -> void:
 	if !sprite_2d: return push_error(self, "Sprite2D is not defined")
 	if !animation_player: return push_error(self, "AnimationPlayer is not defined")
 	if !hurbox_area_2d: return push_error(self, "HurtBoxArea2D is not defined")
 	hurbox_area_2d.body_entered.connect(_on_hurbox_area_2d_body_entered)
 	animation_player.play("idle")
+
+func _physics_process(delta: float) -> void:
+	if movement: movement.accelerate(-1, delta).move()
 
 # method #----------------------------------------------------------------------
 func die() -> void:
